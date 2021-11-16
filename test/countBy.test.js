81cb2e429479f4f71154b0fd7601cf6e25229c22
Janne Vikelä <->
2021-11-16 18:12:26 +0200

    countBy added

---
+++ test/countBy.test.js
@@ -0,0 +1,56 @@
+import countBy from '../src/countBy.js'
+
+// Three different styles
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('Simple countBy test with three values', () => {
+  const users = [
+    { user: 'barney', active: true },
+    { user: 'betty', active: true },
+    { user: 'fred', active: false }]
+
+  // expect
+  it('[],1 -> []', () => {
+    expect(countBy(users, value => value.active)).equal({ true: 2, false: 1 })
+  })
+})
+
+describe('Simple countBy test with four values', () => {
+  const users = [
+    { user: 'barney', active: true },
+    { user: 'betty', active: true },
+    { user: 'billy', active: true },
+    { user: 'fred', active: false }]
+
+  // expect
+  it('[],1 -> []', () => {
+    expect(countBy(users, value => value.active)).equal({ true: 3, false: 1 })
+  })
+})
+
+describe('A countBy test with four equal values', () => {
+  const users = [
+    { user: 'barney', active: true },
+    { user: 'betty', active: true },
+    { user: 'billy', active: true },
+    { user: 'fred', active: true }]
+
+  // expect
+  it('[],1 -> []', () => {
+    expect(countBy(users, value => value.active)).equal({ true: 4 })
+  })
+})
+
+describe('A countBy test with four values and multiple attributes', () => {
+  const users = [
+    { user: 'barney', active: true, age: 30 },
+    { user: 'betty', active: true, age: 30 },
+    { user: 'billy', active: true, age: 25 },
+    { user: 'fred', active: false, age: 27 }]
+
+  // expect
+  it('[],1 -> []', () => {
+    expect(countBy(users, value => value.age)).equal({ 30: 2, 25: 1, 27: 1 })
+  })
+})
