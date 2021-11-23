c19d93b9c55eaf24383f72732dd34d89991439d6
Janne Vikelä <->
2021-11-24 00:00:28 +0200

    words test added

--- test/chunkTest.test.js
+++ test/chunkTest.test.js
@@ -7,42 +7,42 @@ import { describe, it } from 'mocha'
 describe('Empty array chunk', () => {
   // expect
   it('[],1 -> []', () => {
-    expect(chunk([], 1)).equal([])
+    expect(chunk([], 1)).to.deep.equal([])
   })
 })
 
 describe('Simple array chunk', () => {
   // expect
   it('[1,2],1 -> [[1],[2]]', () => {
-    expect(chunk([1, 2], 1)).equal([[1], [2]])
+    expect(chunk([1, 2], 1)).to.deep.equal([[1], [2]])
   })
 })
 
 describe('Char array chunk to size 1', () => {
   // expect
   it('[a,b,c],1 -> [[a],[b],[c]]', () => {
-    expect(chunk(['a', 'b', 'c'], 1)).equal([['a'], ['b'], ['c']])
+    expect(chunk(['a', 'b', 'c'], 1)).to.deep.equal([['a'], ['b'], ['c']])
   })
 })
 
 describe('Char array chunk to size 2', () => {
   // expect
   it('[a,b,c,d],2 -> [[a,b],[c,d]]', () => {
-    expect(chunk(['a', 'b', 'c', 'd'], 2)).equal([['a', 'b'], ['c', 'd']])
+    expect(chunk(['a', 'b', 'c', 'd'], 2)).to.deep.equal([['a', 'b'], ['c', 'd']])
   })
 })
 
 describe('Char array without residual', () => {
   // expect
   it('[a,b,c,d,e,f],3 -> [[a,b,c],[d,e,f]]', () => {
-    expect(chunk(['a', 'b', 'c', 'd', 'e', 'f'], 3)).equal([['a', 'b', 'c'], ['d', 'e', 'f']])
+    expect(chunk(['a', 'b', 'c', 'd', 'e', 'f'], 3)).to.deep.equal([['a', 'b', 'c'], ['d', 'e', 'f']])
   })
 })
 
 describe('Char array with residual', () => {
   // expect
   it('[a,b,c,d,e],3 -> [[a,b,c],[d,e]]', () => {
-    expect(chunk(['a', 'b', 'c', 'd', 'e'], 3)).equal([['a', 'b', 'c'], ['d', 'e']])
+    expect(chunk(['a', 'b', 'c', 'd', 'e'], 3)).to.deep.equal([['a', 'b', 'c'], ['d', 'e']])
   })
 })
 
@@ -50,13 +50,13 @@ describe('Char array with residual', () => {
 describe('Chunk size equal to array size', () => {
   // expect
   it('[a,b,c,d,e],5 -> [a,b,c,d,e]', () => {
-    expect(chunk(['a', 'b', 'c', 'd', 'e'], 5)).equal([['a', 'b', 'c', 'd', 'e']])
+    expect(chunk(['a', 'b', 'c', 'd', 'e'], 5)).to.deep.equal([['a', 'b', 'c', 'd', 'e']])
   })
 })
 
 describe('Chunk size more than array size', () => {
   // expect
   it('[a,b,c,d,e],6 -> [a,b,c,d,e]', () => {
-    expect(chunk(['a', 'b', 'c', 'd', 'e'], 6)).equal([['a', 'b', 'c', 'd', 'e']])
+    expect(chunk(['a', 'b', 'c', 'd', 'e'], 6)).to.deep.equal([['a', 'b', 'c', 'd', 'e']])
   })
 })
--- test/countBy.test.js
+++ test/countBy.test.js
@@ -12,7 +12,7 @@ describe('Simple countBy test with three values', () => {
 
   // expect
   it('[{ ... }] -> {"false": 1, "true": 2}', () => {
-    expect(countBy(users, value => value.active)).equal({ true: 2, false: 1 })
+    expect(countBy(users, value => value.active)).to.deep.equal({ true: 2, false: 1 })
   })
 })
 
@@ -25,7 +25,7 @@ describe('Simple countBy test with four values', () => {
 
   // expect
   it('[{ ... }] -> {"false": 1, "true": 3}', () => {
-    expect(countBy(users, value => value.active)).equal({ true: 3, false: 1 })
+    expect(countBy(users, value => value.active)).to.deep.equal({ true: 3, false: 1 })
   })
 })
 
@@ -38,7 +38,7 @@ describe('A countBy test with four equal values', () => {
 
   // expect
   it('[{ ... }] -> {"true": 4}', () => {
-    expect(countBy(users, value => value.active)).equal({ true: 4 })
+    expect(countBy(users, value => value.active)).to.deep.equal({ true: 4 })
   })
 })
 
@@ -51,6 +51,6 @@ describe('A countBy test with four values and multiple attributes', () => {
 
   // expect
   it('[{ ... }] -> { 30: 2, 25: 1, 27: 1 }', () => {
-    expect(countBy(users, value => value.age)).equal({ 30: 2, 25: 1, 27: 1 })
+    expect(countBy(users, value => value.age)).to.deep.equal({ 30: 2, 25: 1, 27: 1 })
   })
 })
